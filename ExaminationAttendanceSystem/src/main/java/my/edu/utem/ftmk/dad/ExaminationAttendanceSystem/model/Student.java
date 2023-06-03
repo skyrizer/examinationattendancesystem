866986f86caf0037d59ed2edbea2abe34388d473
MOHD HAFIZ SUHAIZAL BIN ISMAIL <hafiz2603@gmail.com>
2023-06-03 13:43:58 +0800

    Update Student.java

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/model/Student.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/model/Student.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//this is for student 
 @Entity
 @Table(name = "student")
 public class Student {
