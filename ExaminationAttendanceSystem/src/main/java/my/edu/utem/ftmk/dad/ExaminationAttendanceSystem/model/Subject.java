d651c465ee06d72dffb2cb1b21336619d2abd7a0
MOHD HAFIZ SUHAIZAL BIN ISMAIL <hafiz2603@gmail.com>
2023-06-03 13:39:51 +0800

    Update Subject.java

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/model/Subject.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/model/Subject.java
@@ -1,19 +1,29 @@
 package my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model;
 
 import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.ArrayList;
 
 public class Subject {
 
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "SubjectId")
+	private String SubjectId;
+	@Column(name = "Name")
 	private String Name;
 	private List<Student> students = new ArrayList<Student>();
 	
-	public String getId() {
-		return id;
+	public String getSubjectId() {
+		return SubjectId;
 	}
-	public void setId(String id) {
-		this.id = id;
+	public void setSubjectId(String subjectId) {
+		SubjectId = subjectId;
 	}
 	public String getName() {
 		return Name;
