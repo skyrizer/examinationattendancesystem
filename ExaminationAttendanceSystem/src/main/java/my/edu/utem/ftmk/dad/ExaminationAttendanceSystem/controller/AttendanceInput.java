3a68029be0431e8ae9892aa97b2dd0051579629b
NG WEI HEN <rolandng0001@gmail.com>
2023-06-03 14:07:53 +0800

    Update AttendanceInput.java (3 June 2023 2:07pm)

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/controller/AttendanceInput.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/controller/AttendanceInput.java
@@ -1,6 +1,8 @@
 package my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.controller;
 
 public interface AttendanceInput {
-
-	public void generateAttendanceInput();
+	public void generateAttendanceInput(LateAttendance lateAttendace);
+	// or public LateAttendance someMethod();
+	
+	public boolean validateAttendanceInput();
 }
