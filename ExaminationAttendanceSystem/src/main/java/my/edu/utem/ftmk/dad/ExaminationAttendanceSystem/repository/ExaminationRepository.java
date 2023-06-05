69863a5e11c2267b07c43d07164b37439fae8166
Ng Wei Hen <rolandng0001@gmail.com>
2023-06-05 19:41:45 +0800

    Update Examination Repository

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/ExaminationRepository.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/ExaminationRepository.java
@@ -1,9 +1,20 @@
 package my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model.Examination;
 
 public interface ExaminationRepository extends JpaRepository<Examination, Long> {
 
+	/*
+	 * This method create custom query for retrieve schedule for students
+	 * 
+	 */
+	@Query(value = "select t.SubjectCode, t.SubjectName, e.ExaminationDate, e.ExaminationTime, e.UnitId "
+			+ "from subject t, examination e, examinationunit u WHERE t.SubjectId = e.SubjectId and e.UnitId = u.UnitId ",
+			nativeQuery = true)
+	public List<Object[]> selectCustomByCode();
 }
