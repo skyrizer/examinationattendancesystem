8e8484274946e835323a1ea5fa2ca9ae257380f2
Ng Wei Hen <rolandng0001@gmail.com>
2023-06-05 19:33:43 +0800

    Update

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/AttendanceRepository.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/AttendanceRepository.java
@@ -3,6 +3,7 @@ package my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model.ExaminationAttendance;
@@ -11,5 +12,6 @@ import my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model.ExaminationAttenda
 public interface AttendanceRepository extends JpaRepository<ExaminationAttendance, Integer> {
 
 	public List<ExaminationAttendance> findById(int id);
+	
 	
 }
