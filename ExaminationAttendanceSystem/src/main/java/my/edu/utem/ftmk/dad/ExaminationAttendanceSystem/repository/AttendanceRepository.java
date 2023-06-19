dec62e1137b9b259ac3627c33e87b94a0d6490d6
skyrizer <wafir2002@gmail.com>
2023-06-19 20:03:28 +0800

    updated attendance report
    add total number of student attend and absent

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/AttendanceRepository.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/repository/AttendanceRepository.java
@@ -3,13 +3,13 @@ package my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model.ExaminationAttendance;
-import my.edu.utem.ftmk.dad.ExaminationAttendanceSystem.model.Student;
+
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<ExaminationAttendance, Long> {
