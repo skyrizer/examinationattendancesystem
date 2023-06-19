a8dd63064688c3a7db2bba2a4e4b48cbfe73c0cd
MOHD HAFIZ SUHAIZAL BIN ISMAIL <125490551+HafizSuhaizal@users.noreply.github.com>
2023-06-20 00:13:10 +0800

    new

--- ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/controller/AttendanceRESTController.java
+++ ExaminationAttendanceSystem/src/main/java/my/edu/utem/ftmk/dad/ExaminationAttendanceSystem/controller/AttendanceRESTController.java
@@ -108,12 +108,10 @@ public class AttendanceRESTController {
 	        String studentName = (String) row[1];
 	        String studentMatricNo = (String) row[2];
 	        String studentCourse = (String) row[3];
-
 	        Student student = new Student();
 	        student.setStudentName(studentName);
 	        student.setStudentMatricNo(studentMatricNo);
 	        student.setStudentCourse(studentCourse);
-
 	        studentList.add(student);
 	    }
 	    
